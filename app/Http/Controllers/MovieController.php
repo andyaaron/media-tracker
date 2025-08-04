d4e88b51a67418bc27c9f343092d5d51bf55cfc6
Aaron <aaron.richard.andy@gmail.com>
2025-08-04 18:15:29 -0400

    - setup api routing - define seearch method

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -6,9 +6,37 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 class MovieController extends Controller
 {
+    private $base_url;
+    private $api_token;
+    private $api_key;
+
+    public function __construct() {
+        $this->base_url = env('TMDB_BASE_URL');
+        $this->api_token = env('TMDB_API_TOKEN');
+        $this->api_key = env('TMDB_API_KEY');
+    }
     // handle the GET /search/movies request
-    public function search() {
-        $response = Http::get('')
+    public function search(Request $request) {
+        // get query param from request
+        $query = $request->query('query');
+
+        // check if param is missinng
+        if (!$query) {
+            return response()->json(['error' => 'Query parameters missing'], 400);
+        }
+
+        $response = Http::get("$this->base_url/search/movie", [
+            'api_key'   => $this->api_key,
+            'query'     => $query,
+        ]);
+
+        // check for success
+        if ($response->successful()) {
+            return $response->json();
+        } else {
+            // log error
+            return response()->json(['error' => 'Failed to retrieve movies from TMDb'], $response->status());
+        }
     }
 
     // read from the database GET /my-list
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
 // Public route for searching movies
