8385803186029bbb426e72f7b623e9b6f45f3032
Aaron <aaron.richard.andy@gmail.com>
2025-08-05 16:12:47 -0400

    setup search function on frontend to make api request setup authorize method

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -8,15 +8,37 @@ use Illuminate\Support\Facades\Log;
 
 class MovieController extends Controller
 {
+    private $app_url;
     private $base_url;
     private $api_token;
     private $api_key;
 
     public function __construct() {
+        $this->app_url = env('APP_URL');
         $this->base_url = env('TMDB_BASE_URL');
         $this->api_token = env('TMDB_API_TOKEN');
         $this->api_key = env('TMDB_API_KEY');
     }
+
+    // generate a session ID for TMDB
+    public function authorize(Request $request) {
+        $query = $request->query();
+
+        $response = Http::withToken($this->api_token)
+            ->get("$this->base_url/authentication/token/new");
+
+        Log::debug("Token: $response");
+
+        if ($response["success"] === true) {
+            $request_token = $response["request_token"];
+
+            $redirect_url = "https://www.themoviedb.org/authenticate/$request_token?redirect_to=$this->app_url#success";
+            return redirect()->away($redirect_url);
+        }
+
+        return response()->json(['error' => 'Failed to get request token.'], 500);
+    }
+
     // handle the GET /search/movies request
     public function search(Request $request) {
         Log::debug("Initiating search!");
@@ -28,8 +50,8 @@ class MovieController extends Controller
             return response()->json(['error' => 'Query parameters missing'], 400);
         }
 
-        $response = Http::withToken($this->api_token)->get("$this->base_url/search/movie", [
-            'api_key'   => $this->api_key,
+        $response = Http::withToken($this->api_token)
+            ->get("$this->base_url/search/movie", [
             'query'     => $query,
         ]);
 
@@ -39,16 +61,37 @@ class MovieController extends Controller
         } else {
             // log error
             return response()->json([
-                'status_code' => $response['status_code'],
-                'status_message' => $response["status_message"],
-                'error' => 'Failed to retrieve movies from TMDb'
+                'status_code'       => $response['status_code'],
+                'status_message'    => $response["status_message"],
+                'error'             => 'Failed to retrieve movies from TMDb'
             ], $response->status());
         }
     }
 
     // read from the database GET /my-list
-    public function index() {
+    public function index(Request $request) {
+        Log::debug("Getting index");
+        $query = $request->query('query');
+        $account_id = $request->query('account_id');
 
+        if (!$query) {
+            return response()->json(['error' => 'Query parameters missing'], 400);
+        }
+
+        $response = Http::withToken($this->api_token)
+            ->get("$this->base_url/account/$account_id/lists", [
+                'page' => 1,
+            ]);
+
+        if ($response->successful()) {
+            return $response->json();
+        } else {
+            return response()->json([
+                'status_code'       => $response['status_code'],
+                'status_message'    => $response["status_message"],
+                'error'             => 'Failed to retrieve list from TMDb'
+            ]);
+        }
     }
 
     // post data
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,12 +1,38 @@
+import {useState} from "react";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard() {
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    const search = async () => {
+        try {
+            const response = await fetch('/api/search/movies?' + new URLSearchParams({ query: query }))
+                .then(response => response.json())
+            if (response.results) {
+                setMovies(response.results);
+            }
+        } catch (error) {
+            console.error("Error fetching movies:", error);
+            setMovies([])
+        }
+    }
+
+    const handleChange = (e) => {
+        setQuery(e.target.value);
+    }
+
+    const handleClick = async () => {
+        await search(query)
+    }
+
+    console.log(movies)
     return (
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
+                    Media Tracker
                 </h2>
             }
         >
@@ -17,6 +43,16 @@ export default function Dashboard() {
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                             You're logged in!
+                        </div>
+                        <input type={"text"} placeholder={"Search for a movie... "} onChange={handleChange}/>
+                        <button onClick={handleClick}>Submit</button>
+
+                        <div className={"search-results"}>
+                            {movies?.length > 1 && (
+                                movies.map(movie => (
+                                        <div>{movie["title"]}</div>
+                                ))
+                            )}
                         </div>
                     </div>
                 </div>
